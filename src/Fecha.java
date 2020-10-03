c5a0e8f51bf7e7d2f2474c325a0ad331d31abef9
“Mike674 <miguelahumadad@hotmail.com>
2020-10-02 22:59:21 -0600

    subiendo los primeros archivos

---
+++ src/Fecha.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GX505DT
+ */
+public class Fecha {
+    private  int dia;
+    private int mes;
+    private  int año;
+    private int hora;
+    private  int minuto;
+    
+
+}
